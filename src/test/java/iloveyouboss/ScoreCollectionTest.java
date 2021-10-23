ff4a8af9110bd1a91df65eaa0b0b86e9a0f9c436
Manish Anand <manishanandprogrammer@gmail.com>
2021-10-23 21:56:41 +0530

    started pragmatic unit testing in java book

---
+++ src/test/java/iloveyouboss/ScoreCollectionTest.java
@@ -0,0 +1,13 @@
+package iloveyouboss;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScoreCollectionTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+}
