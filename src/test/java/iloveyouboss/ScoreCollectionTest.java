7e376a3e73662dce3e3191b21d9a49d84325f082
Manish Anand <manishanandprogrammer@gmail.com>
2021-10-24 07:46:13 +0530

    aaa arrange act assert model. first test

--- src/test/java/iloveyouboss/ScoreCollectionTest.java
+++ src/test/java/iloveyouboss/ScoreCollectionTest.java
@@ -7,7 +7,18 @@ import static org.junit.jupiter.api.Assertions.*;
 class ScoreCollectionTest {
 
     @Test
-    public void test() {
-        fail("Not yet implemented");
+    public void arithmeticMeanOfTwoNumbers() {
+        // arrange
+        ScoreCollection collection = new ScoreCollection();
+        collection.add(() -> 5);
+        collection.add(() -> 7);
+
+        // act
+        int actualResult = collection.arithmeticMean();
+
+        // assert
+        int expectedResult = 6;
+        assertEquals(expectedResult, actualResult);
+
     }
 }
